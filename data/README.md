7838912168c65023c5f6c83e61e046c7031f7078
pgleeson <p.gleeson@gmail.com>
2025-07-08 17:40:26 +0100

    Adding initial python script & example data

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Example data files
+
+Some of these files have been taken from: https://rdr.ucl.ac.uk/articles/dataset/All_Preprocessed_Datasets_for_Gurnani_and_Silver_2021/14364845
---
+++ python/LoadCSVData.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+filename = '../data/example_data.csv'
+
+# Load the CSV file into a numpy array
+# Assumes numerical data and no header row
+data = np.loadtxt(filename, delimiter=',')
+
+print(f'Data has shape: {data.shape} and type: {data.dtype}')
+
+# Plot 
+
+plt.imshow(data.T, aspect='auto', cmap='viridis')
+plt.colorbar()
+plt.xlabel('Column')
+plt.ylabel('Row')
+
+
+plt.title('Plot from CSV file')
+
+if not '-nogui' in sys.argv:
+    plt.show()
